38bf2b68491a1b8a082b30d92df5f11e895722b2
TheSaviourEking <saviour.eking@outlook.com>
2023-09-06 04:49:14 +0100

    add solutions

--- classes/teacher.js
+++ classes/teacher.js
@@ -1,7 +1,26 @@
 const Person = require('./person');
 
 // Your code here
+class Teacher extends Person {
+  constructor(firstname, lastName, subject, yearsOfExperience) {
+    super(firstname, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
 
+  static combinedYearsOfExperience(teachers) {
+    // let sum = 0;
+    // teachers.forEach(el => sum += el.yearsOfExperience);
+    // return sum
+    return teachers.reduce((sum, currentValue) => sum + currentValue.yearsOfExperience, 0);
+  }
+}
+
+
+let teacher1 = new Teacher("susan", "jones", "biology", 5);
+let teacher2 = new Teacher("bobby", "roberts", "math", 15);
+
+console.log(Teacher.combinedYearsOfExperience([teacher1, teacher2]))
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
